3744b5c8c3cd5465cfc9cf200a21726b8d4992fe
nazmussakib42 <mkr.koyra@gmail.com>
2025-07-14 11:02:38 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center mt-10 text-3xl font-bold">
+      Welcome to MKR Enterprise Frontend!
+    </div>
+  );
+}
+
+export default App;
