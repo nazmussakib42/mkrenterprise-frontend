dfedf7f5d4323bedec097e37562d3928065d4061
nazmussakib42 <mkr.koyra@gmail.com>
2025-07-19 11:02:24 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+```js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173
+  }
+});
+```
